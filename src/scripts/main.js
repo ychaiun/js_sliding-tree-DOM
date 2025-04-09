d9875578ddbede1a5d73876bbb05fa0350e7490b
Yurii Chain <yuriichaiun@gmail.com>
2025-04-10 01:29:51 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+for (const list of listItems) {
+  if (list.children.length !== 0) {
+    const span = document.createElement('span');
+
+    span.innerHTML = list.firstChild.textContent.trim();
+
+    list.insertBefore(span, list.firstChild);
+    list.firstChild.nextSibling.remove();
+
+    span.addEventListener('click', showHeader);
+  }
+}
+
+function showHeader() {
+  const spanDisplay = this.nextSibling.style;
+
+  if (spanDisplay.display === 'none') {
+    spanDisplay.display = '';
+  } else {
+    spanDisplay.display = 'none';
+  }
+}
