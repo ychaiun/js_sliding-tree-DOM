72e89404e6b4e67fec0898d2464e5e3d08533444
Yurii Chain <yuriichaiun@gmail.com>
2025-04-10 23:41:20 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const listItems = document.querySelectorAll("li");
+const style = document.createElement("style");
+style.textContent = `
+  .hidden {
+    display: none;
+  }
+`;
+document.head.append(style);
+for (const list of listItems)if (list.children.length !== 0) {
+    const span = document.createElement("span");
+    span.innerHTML = list.firstChild.textContent.trim();
+    list.insertBefore(span, list.firstChild);
+    list.firstChild.nextSibling.remove();
+    span.addEventListener("click", function() {
+        this.nextSibling.classList.toggle("hidden");
+    });
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
